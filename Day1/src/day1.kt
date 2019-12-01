d83151fabf34bc2003b4c266e31aab0fbd969e45
Domen Soklic <domen.soklic@cosylab.com>
2019-12-01 08:59:33 +0100

    Finished day 1.

---
+++ Day1/src/day1.kt
@@ -0,0 +1,41 @@
+package day1
+
+import java.io.File
+
+val FILE_PATH = "input.txt"
+
+fun calculateFuel(mass: Int): Int {
+    return mass / 3 - 2
+}
+
+fun calculateFuelWithAdditionalFuel(mass: Int): Int {
+    var allFuelRequired = 0
+    var massNeededToFuel = mass
+
+    while (true) {
+        val fuel = calculateFuel(massNeededToFuel)
+
+        if (fuel <= 0) {
+            break;
+        }
+
+        allFuelRequired += fuel
+        massNeededToFuel = fuel
+    }
+
+    return allFuelRequired
+}
+
+fun main() {
+    val fileLines: List<Int> = File(FILE_PATH).readLines().map { x -> x.toInt() }
+
+    val fuelPart1 = fileLines.map { mass -> calculateFuel(mass) }
+                             .sum()
+
+    println("Part 1: Fuel required: $fuelPart1")
+
+    val fuelPart2 = fileLines.map { mass -> calculateFuelWithAdditionalFuel(mass) }
+                             .sum()
+
+    println("Part 2: Fuel required: $fuelPart2")
+}
