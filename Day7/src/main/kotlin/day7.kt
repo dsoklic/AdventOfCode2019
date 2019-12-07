86a89a6fa07bb4d2c5468035e947a1a8b7a7d486
Domen Soklic <domen.soklic@cosylab.com>
2019-12-07 14:28:19 +0100

    Reimplemented day 7 part 1 with queues.

--- Day7/src/main/kotlin/day7.kt
+++ Day7/src/main/kotlin/day7.kt
@@ -17,111 +17,133 @@ const val EQUALS = "08"
 const val HALT = "99"
 const val FILE_PATH = "input.txt"
 
-enum class Mode {
-    Position,
-    Immediate
-}
+class Computer(val inputQueue: Queue<Int>, val outputQueue: Queue<Int>, program: List<Int>) {
+    private val programMemory = program.toMutableList()
+    private var index = 0
+    var running = true;
 
-fun executeProgram(inputValue: Queue<Int>, programMemory: MutableList<Int>): Int {
-    var index = 0
-    var outputValue = 0
+    private enum class Mode {
+        Position,
+        Immediate
+    }
 
-    while (true) {
-        val instruction = programMemory[index].toString().padStart(4, '0')
-        val opcode = instruction.substring(2)
+    fun executeProgram() {
+        while (true) {
+            val instruction = programMemory[index].toString().padStart(4, '0')
+            val opcode = instruction.substring(2)
 
-        val input1Mode = if (instruction[1] == '0') Mode.Position else Mode.Immediate
-        val input2Mode = if (instruction[0] == '0') Mode.Position else Mode.Immediate
+            val input1Mode = if (instruction[1] == '0') Mode.Position else Mode.Immediate
+            val input2Mode = if (instruction[0] == '0') Mode.Position else Mode.Immediate
 
-        when (opcode) {
-            ADDITION -> {
-                val inp1 = getInput(programMemory[index+1], input1Mode, programMemory)
-                val inp2 = getInput(programMemory[index+2], input2Mode, programMemory)
-                programMemory[programMemory[index+3]] = inp1 + inp2
-                index += 4
-            }
-            MULTIPLICATION -> {
-                val inp1 = getInput(programMemory[index+1], input1Mode, programMemory)
-                val inp2 = getInput(programMemory[index+2], input2Mode, programMemory)
-                programMemory[programMemory[index+3]] = inp1 * inp2
-                index += 4
-            }
-            INPUT -> {
-                programMemory[programMemory[index+1]] = inputValue.remove()
-                index += 2
-            }
-            OUTPUT -> {
-                outputValue = getInput(programMemory[index+1], input1Mode, programMemory)
-                index += 2
-            }
-
-            JUMP_IF_TRUE -> {
-                val inp = getInput(programMemory[index+1], input1Mode, programMemory)
-                if (inp != 0) {
-                    index = getInput(programMemory[index+2], input2Mode, programMemory)
-                } else {
-                    index += 3
+            when (opcode) {
+                ADDITION -> {
+                    val inp1 = getInput(programMemory[index + 1], input1Mode, programMemory)
+                    val inp2 = getInput(programMemory[index + 2], input2Mode, programMemory)
+                    programMemory[programMemory[index + 3]] = inp1 + inp2
+                    index += 4
                 }
-            }
-            JUMP_IF_FALSE -> {
-                val inp = getInput(programMemory[index+1], input1Mode, programMemory)
-                if (inp == 0) {
-                    index = getInput(programMemory[index+2], input2Mode, programMemory)
-                } else {
-                    index += 3
+                MULTIPLICATION -> {
+                    val inp1 = getInput(programMemory[index + 1], input1Mode, programMemory)
+                    val inp2 = getInput(programMemory[index + 2], input2Mode, programMemory)
+                    programMemory[programMemory[index + 3]] = inp1 * inp2
+                    index += 4
                 }
-            }
-            LESS_THAN -> {
-                val inp1 = getInput(programMemory[index+1], input1Mode, programMemory)
-                val inp2 = getInput(programMemory[index+2], input2Mode, programMemory)
+                INPUT -> {
+                    if (inputQueue.isEmpty()) {
+                        // Queue is empty, stop processing until input is available.
+                        return
+                    }
 
-                programMemory[programMemory[index+3]] = if (inp1 < inp2) 1 else 0
-                index += 4
-            }
-            EQUALS -> {
-                val inp1 = getInput(programMemory[index+1], input1Mode, programMemory)
-                val inp2 = getInput(programMemory[index+2], input2Mode, programMemory)
+                    programMemory[programMemory[index + 1]] = inputQueue.remove()
+                    index += 2
+                }
+                OUTPUT -> {
+                    outputQueue.add(getInput(programMemory[index + 1], input1Mode, programMemory))
+                    index += 2
+                }
 
-                programMemory[programMemory[index+3]] = if (inp1 == inp2) 1 else 0
-                index += 4
-            }
+                JUMP_IF_TRUE -> {
+                    val inp = getInput(programMemory[index + 1], input1Mode, programMemory)
+                    if (inp != 0) {
+                        index = getInput(programMemory[index + 2], input2Mode, programMemory)
+                    } else {
+                        index += 3
+                    }
+                }
+                JUMP_IF_FALSE -> {
+                    val inp = getInput(programMemory[index + 1], input1Mode, programMemory)
+                    if (inp == 0) {
+                        index = getInput(programMemory[index + 2], input2Mode, programMemory)
+                    } else {
+                        index += 3
+                    }
+                }
+                LESS_THAN -> {
+                    val inp1 = getInput(programMemory[index + 1], input1Mode, programMemory)
+                    val inp2 = getInput(programMemory[index + 2], input2Mode, programMemory)
 
-            HALT -> return outputValue
-            else -> throw IllegalArgumentException("Unexpected opcode: '$opcode'")
+                    programMemory[programMemory[index + 3]] = if (inp1 < inp2) 1 else 0
+                    index += 4
+                }
+                EQUALS -> {
+                    val inp1 = getInput(programMemory[index + 1], input1Mode, programMemory)
+                    val inp2 = getInput(programMemory[index + 2], input2Mode, programMemory)
+
+                    programMemory[programMemory[index + 3]] = if (inp1 == inp2) 1 else 0
+                    index += 4
+                }
+
+                HALT -> {
+                    running = false
+                    return
+                }
+                else -> throw IllegalArgumentException("Unexpected opcode: '$opcode'")
+            }
         }
     }
-}
 
-fun getInput(parameter: Int, mode: Mode, memory: List<Int>): Int = when (mode) {
-    Mode.Immediate -> parameter
-    Mode.Position -> memory[parameter]
+    private fun getInput(parameter: Int, mode: Mode, memory: List<Int>): Int = when (mode) {
+        Mode.Immediate -> parameter
+        Mode.Position -> memory[parameter]
+    }
 }
 
 fun String.toProgram(): List<Int> = this.split(',').map{ it.toInt() }.toList()
 
 fun amplifiersPart1(phases: List<Int>, program: List<Int>): Int {
-    var signal = 0
+    val aInpQ = LinkedList<Int>(listOf(phases[0], 0))
+    val abInpQ = LinkedList<Int>(listOf(phases[1]))
+    val bcInpQ = LinkedList<Int>(listOf(phases[2]))
+    val cdInpQ = LinkedList<Int>(listOf(phases[3]))
+    val deInpQ = LinkedList<Int>(listOf(phases[4]))
+    val eOutQ = LinkedList<Int>()
 
-    for (phase in phases) {
-        signal = executeProgram(LinkedList(listOf(phase, signal)), program.toMutableList())
-    }
+    val amplifiers = listOf(
+        Computer(aInpQ, abInpQ, program),
+        Computer(abInpQ, bcInpQ, program),
+        Computer(bcInpQ, cdInpQ, program),
+        Computer(cdInpQ, deInpQ, program),
+        Computer(deInpQ, eOutQ, program)
+    )
 
-    return signal
+    amplifiers.forEach{ it.executeProgram() }
+
+    return eOutQ.remove()
 }
 
-fun amplifiersPart2(phases: List<Int>, program: List<Int>): Int {
-    var signal = 0
-    val ampMemories = (1..5).map { program.toMutableList() }.toList()
-
-    do {
-        val startSignal = signal
-        phases.forEachIndexed { i, phase ->
-            signal = executeProgram(LinkedList(listOf(phase, signal)), ampMemories[i])
-        }
-    } while (startSignal != signal)
-
-    return signal
-}
+//fun amplifiersPart2(phases: List<Int>, program: List<Int>): Int {
+//    var signal = 0
+//    val ampMemories = (1..5).map { program.toMutableList() }.toList()
+//
+//    do {
+//        val startSignal = signal
+//        phases.forEachIndexed { i, phase ->
+//            signal = executeProgram(LinkedList(listOf(phase, signal)), ampMemories[i])
+//        }
+//    } while (startSignal != signal)
+//
+//    return signal
+//}
 
 fun allPhaseCombinations(part1: Boolean): List<List<Int>> {
     var listOfPhases = mutableListOf<List<Int>>()
